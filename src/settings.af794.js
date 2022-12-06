1efb4f66fe886322a79e27c473e1e984b7a08e1a
DDenver <den107620@gmail.com>
2022-12-06 11:22:47 +0300

    build with vconsole

---
+++ src/settings.af794.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["world","UI"],collisionMatrix:[[true],[false,false]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/Main.fire",orientation:"",jsList:[],bundleVers:{internal:"aeb66",main:"971e2"}};
