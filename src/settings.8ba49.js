26fd2cd7fa029bddc1ee677cd43ca7fef4898c71
DDenver <den107620@gmail.com>
2022-12-06 15:22:40 +0300

    build v1.2

---
+++ src/settings.8ba49.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["world","UI"],collisionMatrix:[[true],[false,false]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/Main.fire",orientation:"",jsList:[],bundleVers:{internal:"52d36",main:"8f6ee"}};
