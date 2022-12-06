15137a831898bfd391f6265904df4668238fb5ad
DDenver <den107620@gmail.com>
2022-12-06 11:38:06 +0300

    build v1.01

--- src/settings.af794.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["world","UI"],collisionMatrix:[[true],[false,false]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/Main.fire",orientation:"",jsList:[],bundleVers:{internal:"aeb66",main:"971e2"}};
