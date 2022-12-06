5eaf33950930d2a3a9c2907465c2b7514fecb224
DDenver <den107620@gmail.com>
2022-12-06 12:41:23 +0300

    v1.06

--- src/settings.8ee5f.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["world","UI"],collisionMatrix:[[true],[false,false]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/Main.fire",orientation:"",jsList:[],bundleVers:{internal:"52d36",main:"94ab2"}};
